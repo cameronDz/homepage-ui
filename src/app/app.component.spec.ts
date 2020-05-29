686e40240334edfa506cfeb96c9f658776620d3d
Cam Dziurgot <camerondziurgot@gmail.com>
2020-05-29 03:01:19 -0400

    update test declarations and remove redundant test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,22 @@
 import { async, TestBed, TestModuleMetadata } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CircleCardLinkComponent } from './components/circle-card-link/circle-card-link.component';
+import { CloudFooterComponent } from './components/cloud-footer/cloud-footer.component';
+import { LinkedFooterComponent } from './components/linked-footer/linked-footer.component';
+import { ResourceCardsComponent } from './components/resource-cards/resource-cards.component';
+import { TerminalCardsComponent } from './components/terminal-cards/terminal-cards.component';
+import { ToolbarHeaderComponent } from './components/toolbar-header/toolbar-header.component';
 
 describe('AppComponent', () => {
-  const config: TestModuleMetadata = { declarations: [AppComponent] };
+  const config: TestModuleMetadata = { declarations: [
+    AppComponent,
+    CircleCardLinkComponent,
+    CloudFooterComponent,
+    LinkedFooterComponent,
+    ResourceCardsComponent,
+    TerminalCardsComponent,
+    ToolbarHeaderComponent
+  ] };
   beforeEach(async(() => {
     TestBed.configureTestingModule(config).compileComponents();
   }));
@@ -13,17 +27,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'md-landing-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('md-landing-app');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    const expectd: string = fixture.componentRef.instance.title;
+    const expectd: string = fixture.componentInstance.title;
     expect(compiled.querySelector('.content h1').textContent).toContain(expectd);
   });
 });
