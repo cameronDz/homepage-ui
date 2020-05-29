d122922f4a10e79e197554fb36e962726d5aa6e6
Cam Dziurgot <camerondziurgot@gmail.com>
2020-05-28 20:31:11 -0400

    generate a header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CloudFooterComponent } from './components/cloud-footer/cloud-footer.component';
+import { ToolbarHeaderComponent } from './components/toolbar-header/toolbar-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CloudFooterComponent
+    CloudFooterComponent,
+    ToolbarHeaderComponent
   ],
   imports: [
     BrowserModule
