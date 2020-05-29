25a6d6ad2c90650ce083e014f6bed10eebf26353
Cam Dziurgot <camerondziurgot@gmail.com>
2020-05-28 20:49:25 -0400

    move circle card link to own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CloudFooterComponent } from './components/cloud-footer/cloud-footer.component';
 import { ToolbarHeaderComponent } from './components/toolbar-header/toolbar-header.component';
+import { CircleCardLinkComponent } from './components/circle-card-link/circle-card-link.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CloudFooterComponent,
-    ToolbarHeaderComponent
+    ToolbarHeaderComponent,
+    CircleCardLinkComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/circle-card-link/circle-card-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'md-circle-card-link',
+  templateUrl: './circle-card-link.component.html',
+  styleUrls: ['./circle-card-link.component.scss']
+})
+export class CircleCardLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
