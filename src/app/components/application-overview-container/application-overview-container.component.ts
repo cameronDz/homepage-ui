fd6cb47269e60562319877387f5e332b8290af05
Cam Dziurgot <camerondziurgot@gmail.com>
2020-06-18 01:14:09 -0400

    add container class and service calls

--- src/app/components/application-overview-container/application-overview-container.component.ts
+++ src/app/components/application-overview-container/application-overview-container.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ApplicationOverviewService } from '../application-overview/application-overview.service';
+import { BulletPointService } from '../bullets-container/bullet-point.service';
+import { ApplicationOverviewModel } from '../application-overview/application-overview.model';
+import { BulletPointModel } from '../bullets-container/bullet-point.model';
 
 @Component({
   selector: 'md-application-overview-container',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationOverviewContainerComponent implements OnInit {
 
-  constructor() { }
+  public overviewModel: ApplicationOverviewModel;
+  public technologyBullets: Array<BulletPointModel>;
+
+  constructor(private applicationOverviewService: ApplicationOverviewService, private bulletPointService: BulletPointService) {}
 
   ngOnInit(): void {
+    this.overviewModel = this.applicationOverviewService.getAugmentedApplicationInformation();
+    this.technologyBullets = this.bulletPointService.getAugmentedApplicationBulletPoints();
+    console.log('this.overviewModel', this.overviewModel);
   }
 
 }
