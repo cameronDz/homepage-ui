0067e07873950b99df3293d6e093bceb7df83983
Cam Dziurgot <camerondziurgot@gmail.com>
2020-06-17 23:57:59 -0400

    add access to application bullets in service

--- src/app/components/bullets-container/bullet-point.service.ts
+++ src/app/components/bullets-container/bullet-point.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BulletPointModel } from './bullet-point.model';
 import * as _overviewBullets from '../../../assets/data/overviewBullets.json';
+import * as _augmentedApplicationBullets from '../../../assets/data/augmentedApplicationBullets.json';
+import * as _logNotesApplicationBullets from '../../../assets/data/logNotesApplicationBullets.json';
 
 @Injectable({ providedIn: 'root' })
 export class BulletPointService {
@@ -9,9 +11,29 @@ export class BulletPointService {
 
   public getOverviewBulletPoints(): Array<BulletPointModel> {
     let array: Array<any> = [];
-    if (_overviewBullets && _overviewBullets.payload && Array.isArray(_overviewBullets.payload.bullets)) {
+    if (this.isValidPayload(_overviewBullets)) {
       array = _overviewBullets.payload.bullets;
     }
     return array;
+  }
+
+  public getAugmentedApplicationBulletPoints(): Array<BulletPointModel> {
+    let array: Array<any> = [];
+    if (this.isValidPayload(_augmentedApplicationBullets)) {
+      array = _augmentedApplicationBullets.payload.bullets;
+    }
+    return array;
+  }
+
+  public getLogNotesApplicationBulletPoints(): Array<BulletPointModel> {
+    let array: Array<any> = [];
+    if (this.isValidPayload(_logNotesApplicationBullets)) {
+      array = _logNotesApplicationBullets.payload.bullets;
+    }
+    return array;
+  }
+
+  private isValidPayload(root: any): boolean {
+    return root && root.payload && Array.isArray(root.payload.bullets);
   }
 }
