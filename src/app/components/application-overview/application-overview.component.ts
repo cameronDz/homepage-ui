372fe9127d73b440dedac81f514a7d9a3a7ef430
Cam Dziurgot <camerondziurgot@gmail.com>
2020-06-18 23:21:48 -0400

    update app overview to include default deploy and tech list lines

--- src/app/components/application-overview/application-overview.component.ts
+++ src/app/components/application-overview/application-overview.component.ts
@@ -8,8 +8,11 @@ import { BulletPointModel } from '../bullets-container/bullet-point.model';
 })
 export class ApplicationOverviewComponent implements OnInit {
 
+  public readonly TECHNOLOGY_LIST_TEXT: string = 'List of related pages associate with the application:';
   public readonly TECHNOLOGY_TITLE: string = 'Technologies';
+  public readonly VISIT_DEPLOYED_APPLICATION_TEXT: string = 'Visit the deployed application here;';
 
+  @Input() deployedUrl: string = '';
   @Input() overviewParagraphs: Array<string> = [];
   @Input() technologyBullets: Array<BulletPointModel> = [];
   @Input() technologyParagraphs: Array<string> = [];
--- src/app/components/application-overview/application-overview.model.ts
+++ src/app/components/application-overview/application-overview.model.ts
@@ -1,9 +1,14 @@
 export class ApplicationOverviewModel {
+  public deployedUrl: string;
   public overviewParagraphs: Array<string>;
   public technologyParagraphs: Array<string>;
   public title: string;
 
-  constructor(title: string = '', overviewParagraphs: Array<string> = [], technologyParagraphs: Array<string> = []) {
+  constructor(title: string = '',
+              overviewParagraphs: Array<string> = [],
+              technologyParagraphs: Array<string> = [],
+              deployedUrl: string = '') {
+    this.deployedUrl = deployedUrl;
     this.overviewParagraphs = overviewParagraphs;
     this.technologyParagraphs = technologyParagraphs;
     this.title = title;
