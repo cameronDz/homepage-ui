bb64bce7e155001ccf1a2fc8b80aa706e3cfa7ac
cameronDz <camerondziurgot@gmail.com>
2021-09-11 14:56:57 -0400

    rm services and subscriptions and replace with input data

--- src/app/components/application-overview-container/application-overview-container.component.ts
+++ src/app/components/application-overview-container/application-overview-container.component.ts
@@ -1,13 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-
-import { ApplicationOverviewService } from '../../services/application-overview.service';
-import { BulletPointService } from '../../services/bullet-point.service';
-import { SubscriptionUtility } from '../../utilities/subscription.utility';
-
-import { ApplicationName, ApplicationOverviewModel } from '../../models/application-overview.model';
-import { BulletPointModel, BulletPointType } from '../../models/bullet-point.model';
-import { DataOptions } from '../../models/data-options.model';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ApplicationOverviewModel } from '../../models/application-overview.model';
 
 @Component({
   selector: 'nssd-application-overview-container',
@@ -16,104 +8,12 @@ import { DataOptions } from '../../models/data-options.model';
 })
 export class ApplicationOverviewContainerComponent implements OnDestroy, OnInit {
 
-  public readonly APPLICATIONS_HEADER: string = 'Deployed Applications';
+  @Input() applications: Array<ApplicationOverviewModel> = [];
+  @Input() title: string = 'Deployed Applications';
 
-  public augmentedOverviewModel: ApplicationOverviewModel;
-  public augmentedTechnologyBullets: Array<BulletPointModel>;
+  constructor() {}
 
-  public logNotesOverviewModel: ApplicationOverviewModel;
-  public logNotesTechnologyBullets: Array<BulletPointModel>;
+  ngOnInit(): void {}
 
-  private augmentedApplicationBulletPointsDataSubscription: Subscription = null;
-  private augmentedApplicationInformationDataSubscription: Subscription = null;
-  private logNotesApplicationBulletPointsDataSubscription: Subscription = null;
-  private logNotesApplicationInformationDataSubscription: Subscription = null;
-
-  constructor(private applicationOverviewService: ApplicationOverviewService, private bulletPointService: BulletPointService) {}
-
-  ngOnInit(): void {
-    this.retieveAugmentedApplicationBullets();
-    this.retieveAugmentedApplicationInformation();
-    this.retieveLogNotesApplicationBullets();
-    this.retieveLogNotesApplicationInformation();
-  }
-
-  ngOnDestroy(): void {
-    SubscriptionUtility.unsubscribe(this.augmentedApplicationBulletPointsDataSubscription);
-    SubscriptionUtility.unsubscribe(this.augmentedApplicationInformationDataSubscription);
-    SubscriptionUtility.unsubscribe(this.logNotesApplicationBulletPointsDataSubscription);
-    SubscriptionUtility.unsubscribe(this.logNotesApplicationInformationDataSubscription);
-  }
-
-  private retieveAugmentedApplicationInformation(): void {
-    SubscriptionUtility.unsubscribe(this.augmentedApplicationInformationDataSubscription);
-    this.augmentedApplicationInformationDataSubscription = this.applicationOverviewService.retrieveApplicationInformation(
-      this,
-      new DataOptions(null, ApplicationName.AUGMENTED),
-      this.successfulAugmentedApplicationInformationCallback,
-      this.errorAugmentedApplicationInformationCallback,
-      this.completedAugmentedApplicationInformationCallback);
-  }
-
-  private successfulAugmentedApplicationInformationCallback(self: ApplicationOverviewContainerComponent, data: ApplicationOverviewModel): void {
-    self.augmentedOverviewModel = data;
-  }
-
-  // TODO implement when retrieving data from API
-  private errorAugmentedApplicationInformationCallback(self: ApplicationOverviewContainerComponent, error: any): void {}
-  private completedAugmentedApplicationInformationCallback(self: ApplicationOverviewContainerComponent): void {}
-
-  private retieveAugmentedApplicationBullets(): void {
-    SubscriptionUtility.unsubscribe(this.augmentedApplicationBulletPointsDataSubscription);
-    this.augmentedApplicationBulletPointsDataSubscription = this.bulletPointService.getBulletPoints(
-      this,
-      new DataOptions(BulletPointType.AUGMENTED_APP),
-      this.successfulAugmentedApplicationBulletsCallback,
-      this.errorAugmentedApplicationBulletsCallback,
-      this.completedAugmentedApplicationBulletsCallback);
-  }
-
-  private successfulAugmentedApplicationBulletsCallback(self: ApplicationOverviewContainerComponent, data: Array<BulletPointModel>): void {
-    self.augmentedTechnologyBullets = data;
-  }
-
-  // TODO implement when retrieving data from API
-  private errorAugmentedApplicationBulletsCallback(self: ApplicationOverviewContainerComponent, error: any): void {}
-  private completedAugmentedApplicationBulletsCallback(self: ApplicationOverviewContainerComponent): void {}
-
-  private retieveLogNotesApplicationInformation(): void {
-    SubscriptionUtility.unsubscribe(this.logNotesApplicationInformationDataSubscription);
-    this.logNotesApplicationInformationDataSubscription = this.applicationOverviewService.retrieveApplicationInformation(
-      this,
-      new DataOptions(null, ApplicationName.ARTICLE_NOTES),
-      this.successfulLogNotesApplicationInformationCallback,
-      this.errorLogNotesApplicationInformationCallback,
-      this.completedLogNotesApplicationInformationCallback);
-  }
-
-  private successfulLogNotesApplicationInformationCallback(self: ApplicationOverviewContainerComponent, data: ApplicationOverviewModel): void {
-    self.logNotesOverviewModel = data;
-  }
-
-  // TODO implement when retrieving data from API
-  private errorLogNotesApplicationInformationCallback(self: ApplicationOverviewContainerComponent, error: any): void {}
-  private completedLogNotesApplicationInformationCallback(self: ApplicationOverviewContainerComponent): void {}
-
-  private retieveLogNotesApplicationBullets(): void {
-    SubscriptionUtility.unsubscribe(this.logNotesApplicationBulletPointsDataSubscription);
-    this.logNotesApplicationBulletPointsDataSubscription = this.bulletPointService.getBulletPoints(
-      this,
-      new DataOptions(BulletPointType.ARTICLE_NOTES_APP),
-      this.successfulLogNotesApplicationBulletsCallback,
-      this.errorAugmentedLogNotesApplicationBulletsCallback,
-      this.completedLogNotesApplicationBulletsCallback);
-  }
-
-  private successfulLogNotesApplicationBulletsCallback(self: ApplicationOverviewContainerComponent, data: Array<BulletPointModel>): void {
-    self.logNotesTechnologyBullets = data;
-  }
-
-  // TODO implement when retrieving data from API
-  private errorAugmentedLogNotesApplicationBulletsCallback(self: ApplicationOverviewContainerComponent, error: any): void {}
-  private completedLogNotesApplicationBulletsCallback(self: ApplicationOverviewContainerComponent): void {}
+  ngOnDestroy(): void {}
 }
