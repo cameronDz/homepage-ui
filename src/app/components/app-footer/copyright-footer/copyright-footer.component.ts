da6cd542b93ff3ab1a154b768a4523f6e4c0f996
cameronDz <camerondziurgot@gmail.com>
2021-09-11 19:42:32 -0400

    add footer details

--- src/app/components/app-footer/copyright-footer/copyright-footer.component.ts
+++ src/app/components/app-footer/copyright-footer/copyright-footer.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import _packageDetails from "../../../../../package.json";
 @Component({
   selector: 'nssd-copyright-footer',
   templateUrl: './copyright-footer.component.html',
   styleUrls: ['./copyright-footer.component.scss']
 })
 export class CopyrightFooterComponent implements OnInit {
-
-  private readonly COPYRIGHT_NAME: string = 'Cam Dziurgot';
   public copyrightText: string = '';
 
   constructor() {}
 
   ngOnInit(): void {
-    const year: number = new Date().getFullYear();
-    this.copyrightText = year + ' ' + this.COPYRIGHT_NAME;
+    const displayName: string = !!_packageDetails?.author?.name ? `, ${_packageDetails.author.name}` : '';
+    const displayVersion: string = !!_packageDetails?.version ? `. v${_packageDetails.version}` : '';
+    const displayYear: number = new Date().getFullYear();
+    this.copyrightText = `${displayYear}${displayName}${displayVersion}`;
   }
-
 }
