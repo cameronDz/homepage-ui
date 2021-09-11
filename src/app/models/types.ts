3648a5685fecd61f742143d836339f9e640a71e8
cameronDz <camerondziurgot@gmail.com>
2021-09-11 13:31:33 -0400

    add new service

---
+++ src/app/models/types.ts
@@ -0,0 +1,3 @@
+export declare type onCompleteCallback = (self: any) => void;
+export declare type onErrorCallback = (self: any, error: any) => void;
+export declare type onSuccessCallback = (self: any, data: any) => void;
---
+++ src/app/services/https-request.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { onCompleteCallback, onErrorCallback, onSuccessCallback } from '../models/types';
+
+
+@Injectable({ providedIn: 'root' })
+export class HttpsRequestService {
+
+    private readonly placeholder: string = '${endpoint}';
+    private readonly GET_URL: string = 'https://homepage-storage-api.herokuapp.com/json/object/${endpoint}';
+
+    constructor(private httpClient: HttpClient) {}
+
+    public getData(
+          self: any,
+          source: string,
+          successCallback: onSuccessCallback,
+          errorCallback: onErrorCallback,
+          completedCallback: onCompleteCallback): Subscription {
+        return this.httpClient.get(this.GET_URL.replace(this.placeholder, source), {})
+            .pipe(finalize((): void => {
+                completedCallback(self);
+            }))
+            .subscribe((data: any): void => {
+                successCallback(self, data);
+            },
+            (error: any): void => {
+                errorCallback(self, error);
+            });
+    }
+}
