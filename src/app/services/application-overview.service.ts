85c0edfcc57c4fb8146c02a3367adaf8b9de9418
cameronDz <camerondziurgot@gmail.com>
2021-09-11 15:07:46 -0400

    update service

--- src/app/services/application-overview.service.ts
+++ src/app/services/application-overview.service.ts
@@ -3,10 +3,7 @@ import { Injectable } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { HttpsRequestService } from './https-request.service';
-import { ApplicationName, ApplicationOverviewModel } from '../models/application-overview.model';
-import { DataOptions } from '../models/data-options.model';
 import { onCompleteCallback, onErrorCallback, onSuccessCallback } from "../models/types";
-import _applicationOverviews from '../../assets/data/applicationOverviews.json';
 
 @Injectable({ providedIn: 'root' })
 export class ApplicationOverviewService extends HttpsRequestService {
@@ -22,37 +19,5 @@ export class ApplicationOverviewService extends HttpsRequestService {
       errorCallback: onErrorCallback,
       completedCallback: onCompleteCallback): Subscription {
     return this.getData(self, this.ALL_DATA_SOURCE, successCallback, errorCallback, completedCallback)
-  }
-
-  public retrieveApplicationInformation(self: any, options: DataOptions, successCallback: any, errorCallback: any, completedCallback: any): Subscription {
-    if (!!options && options.applicationName === ApplicationName.AUGMENTED) {
-      successCallback(self, this.getApplicationInformation(ApplicationName.AUGMENTED));
-    } else if (!!options && options.applicationName === ApplicationName.ARTICLE_NOTES) {
-      successCallback(self, this.getApplicationInformation(ApplicationName.ARTICLE_NOTES));
-    } else {
-      errorCallback(self, 'INVALID-OPTIONS');
-    }
-    completedCallback(self);
-    return null;
-  }
-
-  public getAugmentedApplicationInformation(): ApplicationOverviewModel {
-    return this.getApplicationInformation(ApplicationName.AUGMENTED);
-  }
-
-  public getLogNotesApplicationInformation(): ApplicationOverviewModel {
-    return this.getApplicationInformation(ApplicationName.ARTICLE_NOTES);
-  }
-
-  private getApplicationInformation(applicationName: ApplicationName): ApplicationOverviewModel {
-    let model: ApplicationOverviewModel = new ApplicationOverviewModel();
-    if (this.hasApplicationModel(_applicationOverviews, applicationName)) {
-      model = _applicationOverviews.payload[applicationName];
-    }
-    return model;
-  }
-
-  private hasApplicationModel(root: any, applicationName: ApplicationName): boolean {
-    return root?.payload && root.payload[applicationName];
   }
 }
